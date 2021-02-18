5f51c6f0c341fa032a1b1e15580ee0b6c9732333
siddiq <siddiq.aiub@gmail.com>
2021-02-18 20:56:11 +0600

    remove some code complexity

--- main.js
+++ main.js
@@ -32,22 +32,18 @@ const getSetRadioButtonValue = (inputID, outputID) => {
     }
 }
 const getInputValue = () => {
-    let name = callInputID('name-display');
-    let age = parseFloat(callInputID('age-display'));
-    let annualIncome = parseFloat(callInputID('annual-income-display'));
-    let meetGoalAge = parseFloat(callInputID('meet-goal-at-age-display'));
-    let goalCost = parseFloat(callInputID('goal-cost-display'));
-    let possibleInvestment = parseFloat(callInputID('possible-investment-display'));
-    let risk = callInputID('risk-display');
-    console.log(risk);
-
+    const getInputValues = Array.from(document.getElementsByClassName('display-output'));
+    let storeInputValues = [];
+    getInputValues.forEach(getValue => storeInputValues.push(getValue.innerHTML));
+    [yourName, age,annualIncome, atAge, presentCost, possibleInvestment,risk] = storeInputValues;
+         
     const inputValues = {
-        name: name,
-        age: age,
-        annualIncome: annualIncome,
-        ageGoal: meetGoalAge,
-        goalCost: goalCost,
-        possibleInvestment: possibleInvestment,
+        name: yourName,
+        age: parseFloat(age),
+        annualIncome: parseFloat(annualIncome),
+        ageGoal: parseFloat(atAge),
+        goalCost: parseFloat(presentCost),
+        possibleInvestment: parseFloat(possibleInvestment),
         risk: risk,
     }
     console.log(inputValues);
